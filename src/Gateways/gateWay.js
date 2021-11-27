29e907ae1e888fb2d623e113001930fe291302a7
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-11-27 11:52:27 +0200

    test

--- src/Gateways/gateWay.js
+++ src/Gateways/gateWay.js
@@ -1,20 +1,22 @@
 export const fetchRandomJoke = category =>
   fetch(`http://api.icndb.com/jokes/random?limitTo=[${category}]`)
     .then(response => {
-      if (response.ok) {
-        return response.json();
+      if (!response.ok) {
+        throw new Error(`Response status is ${response.status}`);
       }
+      return response.json();
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error('Could not fetch ', error));
 
 export const fetchAllJokes = () =>
   fetch('http://api.icndb.com/jokes')
     .then(response => {
-      if (response.ok) {
-        return response.json();
+      if (!response.ok) {
+        throw new Error(`Response status is ${response.status}`);
       }
+      return response.json();
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error('Could not fetch', error));
 
 export const randomSearchFunc = arr => {
   const max = Math.floor(Math.random() * (arr.length - 4)) + 5;
